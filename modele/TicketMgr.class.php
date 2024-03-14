e5f9820238d920e198a3483c5ee3c2c2cd6296e1
bouchra <bouchra.laaz@gmail.com>
2024-03-14 13:33:38 +0100

    requete recherche ticket+ amelioration visu form

--- modele/TicketMgr.class.php
+++ modele/TicketMgr.class.php
@@ -2,20 +2,57 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
-    function searchTicket(?int $idTicket, ?int $idCommande, ?int $codeClt, ?string $nomClt, ?string $statutTicket, ?int $idFact, ?DateTime $dateTicket) {
+    require_once "BDDMgr.class.php";
+    
+        function searchTicket(?int $idTicket, ?int $idCommande, ?string $nomClt, ?string $statutTicket, ?int $idFact, ?string $dateTicket, ?string $idDossier) {
+            $queryParams = [];
             $bdd = BDDMgr::getBDD();
-            $sql = "SELECT C.numCommande, numFact, nomClient, T.idTicketSAV, statutTicket, dateTicket, idDossier, nomUtilisateur FROM `Commande` C
+
+            $sql = "SELECT T.idTicketSAV, C.numCommande, nomClient, statutTicket, numFact, dateTicket, idDossier, nomUtilisateur FROM `Commande` C
             INNER JOIN Client Clt ON Clt.idClient = C.idClient
             LEFT JOIN Facture F ON F.numCommande = C.numCommande
             LEFT JOIN Ticket T ON C.numCommande = T.numCommande
             LEFT JOIN Utilisateur U ON U.idUtilisateur = T.idUtilisateur
-            WHERE T.idTicketSAV LIKE ? AND C.numCommande LIKE ? AND C.idClient LIKE ? AND nomClient LIKE ? AND statutTicket LIKE ? AND numFact LIKE ? AND dateTicket LIKE ?";
+            WHERE 1=1 ";
+            if ($idTicket != null){
+                $sql .= "AND T.idTicketSAV LIKE ? ";
+                $queryParams[]='%'.$idTicket.'%';
+            }
+            if ($idCommande != null){
+                $sql .= "AND C.numCommande LIKE ? ";
+                $queryParams[]='%'.$idCommande.'%';
+            } 
+            if ($nomClt != null) {
+                $sql .= "AND nomClient LIKE ? ";
+                $queryParams[]='%'.$nomClt.'%';
+            }
+            if ($statutTicket != null){
+                $sql .= "AND statutTicket LIKE ? ";
+                $queryParams[]='%'.$statutTicket.'%';
+            }
+            if ($idFact != null){
+                $sql .= "AND numFact LIKE ? ";
+                $queryParams[]='%'.$idFact.'%';
+            }
+            if ($dateTicket != null){
+                //TODO à revoir 
+                $sql .= "AND dateTicket = STR_TO_DATE('".$dateTicket."', '%d/%m/%Y')";
+                //$queryParams[]= "STR_TO_DATE('".$dateTicket."', '%d/%m/%Y')";
+            }
+            if ($idDossier != null){
+                $sql .= "AND idDossier LIKE ?";
+                $queryParams[]='%'.$idDossier.'%';
+            }
+
             $resultat = $bdd->prepare($sql);
-            $resultat->execute(array('%'.$idTicket.'%','%'.$idCommande.'%','%'.$codeClt.'%','%'.$nomClt.'%', '%'.$statutTicket.'%','%'.$idFact.'%', '%'.$dateTicket.'%'  ));
+
+            $resultat->execute($queryParams);
+
             $tResultat = $resultat->fetchAll(PDO::FETCH_ASSOC);
+            echo $sql;
             return $tResultat;
     }
-
-    $result = searchTicket(1,null,null,null,null,null,null);
-    var_dump($result);
+    
+    $test = searchTicket(null,null,null,null,null,"13/03/2024", null);?>
+    <pre><?=var_dump($test)?></pre>
+    
