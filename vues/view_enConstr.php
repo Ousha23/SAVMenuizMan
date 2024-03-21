4d6e78ef19726c17f63f6e003c03e592bedd0a65
bouchra <bouchra.laaz@gmail.com>
2024-03-21 22:07:24 +0100

    ajout btn retour view_enConstr

--- vues/view_enConstr.php
+++ vues/view_enConstr.php
@@ -1,6 +1,8 @@
 <?php ob_start(); ?>
 <main>
-    
+<div class="col-md-6">
+    <a href="index.php?action=dashboard" class="btn btn-primary custom-submit-btn ">Retour à la page d'accueil</a>
+</div>
 </main>
 <?php
 $connexion = $_SESSION['nomUtilisateur'];
